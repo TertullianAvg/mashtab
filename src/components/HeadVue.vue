9d7a13d62ec5cf9efbbd18e4d1873a784d73de8f
TertullianAvg <97131285+TertullianAvg@users.noreply.github.com>
2022-06-04 21:39:35 +0300

    master

---
+++ src/components/HeadVue.vue
@@ -0,0 +1,72 @@
+<template>
+   <header class="header">
+                <a href="" class="header__logo">
+                    <img src="../assets/logo.png" alt="logo">
+                </a>
+                <nav class="header__menu">
+                    <a href="">О компании</a>
+                    <a href="">Техника</a>
+                    <a href="">Сервис</a>
+                    <a href="">Запчасти</a>
+                    <a href="">Контакты</a>
+                </nav>
+                <div class="header__button">
+                    <a href="" class="btn">Войти</a>
+                </div>   
+            </header>
+</template>
+
+<script>
+export default {
+  name: 'HeadVue',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+    margin: 0 170px;
+    padding: 0 15px;
+    display: flex;
+    align-items: center;
+    padding: 24px 0;
+}
+.header__logo {
+    margin-right: 140px;
+}
+.header__menu {
+    display: flex;
+    max-width: 700px;
+    width: 100%;
+    justify-content: space-between;
+}
+.header__menu a {
+    font-weight: 400;
+    font-size: 24px;
+    color:rgb(191, 41, 41);
+}
+.header__menu a:hover {
+    color:rgb(157, 18, 18);
+}
+.header__button {
+    margin-left: auto;
+}
+.btn {
+    display: inline-block;
+    width: 225px;
+    background-color: rgb(191, 41, 41);
+    text-align: center;
+    padding: 20px 0;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 100%;
+    color: #fff;
+    border-radius: 8px;
+}
+.btn:hover {
+    background-color:rgb(157, 18, 18);
+}
+</style>
